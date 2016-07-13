4591d2a993ece4340fadb3d328bdce2a0e791894
arn354 <arn354@email.de>
2016-07-13 08:16:39 +0200

    Revert "Videomode - remove missed lines of previous commit"
    This reverts commit a0461fa96c0edfebf593a6672c0c1dbc94e783e4.

--- lib/python/Plugins/SystemPlugins/Videomode/plugin.py
+++ lib/python/Plugins/SystemPlugins/Videomode/plugin.py
@@ -190,6 +190,10 @@ class AudioSetup(Screen, ConfigListScreen):
 		self["description"] = Label("")
 
 		self.createSetup()
+		self.onLayoutFinish.append(self.layoutFinished)
+
+	def layoutFinished(self):
+		self.setTitle(self.setup_title)
 
 	def startHotplug(self):
 		self.hw.on_hotplug.append(self.createSetup)
