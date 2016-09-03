1385ebe5bd1750dae1a45c7bf1b8a4109d738236
Dimitrij <Dima-73@inbox.lv>
2016-09-03 19:21:51 +0300

    [SimpleChannelSelection] fix set title sreen (#393)

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -2543,7 +2543,8 @@ class SimpleChannelSelection(ChannelSelectionBase, SelectionEventInfo):
 				"keyTV": self.setModeTv,
 			})
 		self.bouquet_mark_edit = OFF
-		self.title = title
+		if isinstance(title, str):
+			self.maintitle = title
 		self.currentBouquet = currentBouquet
 		self.returnBouquet = returnBouquet
 		self.setService = setService
