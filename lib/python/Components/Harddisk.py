dc9c4a9dc57dd318d0f295cd777f596849385b2e
littlesat <littlesat99@yahoo.com>
2016-09-08 15:04:36 +0200

    [Harddisk] hide mmc in general (for hd51, hd52 and solo4K)

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -716,10 +716,7 @@ class HarddiskManager:
 				dev = int(readFile(devpath + "/dev").split(':')[0])
 			else:
 				dev = None
-			if getMachineBuild() in ('gb7252','vusolo4k','hd51','hd52'):
-				devlist = [1, 7, 31, 253, 254, 179] # ram, loop, mtdblock, romblock, ramzswap, mmc
-			else:
-				devlist = [1, 7, 31, 253, 254] # ram, loop, mtdblock, romblock, ramzswap
+			devlist = [1, 7, 31, 253, 254, 179] # ram, loop, mtdblock, romblock, ramzswap, mmc
 			if dev in devlist:
 				blacklisted = True
 			if blockdev[0:2] == 'sr':
