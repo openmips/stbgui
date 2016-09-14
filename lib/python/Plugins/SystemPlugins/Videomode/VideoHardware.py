8111880ed83bbcaa5df07e5385a5dc8e644fd9d4
arn354 <arn354@email.de>
2016-09-14 14:32:46 +0200

    VideoHardware - add bcm73625

--- lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
@@ -40,7 +40,7 @@ class VideoHardware:
 
 	rates["576p"] =			{ "50Hz": 	{ 50: "576p" } }
 
-	if chipset in ('bcm7358', 'bcm7346', 'bcm7356', 'bcm7405', 'bcm7362'):
+	if chipset in ('bcm7358', 'bcm7346', 'bcm7356', 'bcm7405', 'bcm7362', 'bcm73625'):
 		rates["720p"] =		{ "24Hz": 	{ 24: "720p24" },
 									"25Hz": 	{ 25: "720p25" },
 									"30Hz": 	{ 30: "720p30" },
@@ -61,7 +61,7 @@ class VideoHardware:
 									"25Hz":		{ 25: "1080p25" },
 									"30Hz":		{ 30: "1080p30" } }
 
-	elif chipset in ('bcm7358', 'bcm7346', 'bcm7356', 'bcm7362'):
+	elif chipset in ('bcm7358', 'bcm7346', 'bcm7356', 'bcm7362', 'bcm73625'):
 		rates["1080p"] =	{ "50Hz": 	{ 50: "1080p50" },
 									"60Hz": 	{ 60: "1080p" },
 									"multi": 	{ 50: "1080p50", 60: "1080p" } }
@@ -91,7 +91,7 @@ class VideoHardware:
 	modes["Scart"] = ["PAL", "NTSC", "Multi"]
 	modes["DVI-PC"] = ["PC"]
 	
-	if chipset in ('bcm7358', 'bcm7346', 'bcm7356', 'bcm7405', 'bcm7362'):
+	if chipset in ('bcm7358', 'bcm7346', 'bcm7356', 'bcm7405', 'bcm7362', 'bcm73625'):
 		modes["YPbPr"] = ["720p", "1080i", "1080p", "576p", "480p", "576i", "480i"]
 		modes["DVI"] = ["720p", "1080i", "1080p", "576p", "480p", "576i", "480i"]
 		# modes["DVI"] = ["720p", "1080p", "2160p", "1080i", "576p", "480p", "576i", "480i"]
