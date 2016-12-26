c511e74404283688b4f2d1ce2329a8fbb5587614
arn354 <arn354@email.de>
2016-12-26 19:00:16 +0100

    ebase - fix last commit

--- lib/base/ebase.cpp
+++ lib/base/ebase.cpp
@@ -252,18 +252,6 @@ int eMainloop::processOneEvent(unsigned int twisted_timeout, PyObject **res, ePy
 
 	ret = _poll(pfd, fdcount, poll_timeout);
 
-	if (this == eApp)
-	{
-		Py_BEGIN_ALLOW_THREADS
-		{
-			ret = ::poll(pfd, fdcount, poll_timeout);
-		}
-		Py_END_ALLOW_THREADS
-	} else
-		ret = ::poll(pfd, fdcount, poll_timeout);
-
-	m_is_idle = 0;
-
 	/* ret > 0 means that there are some active poll entries. */
 	if (ret > 0)
 	{
