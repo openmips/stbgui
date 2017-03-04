73098942a63a200937b977330ff525a74fd1f0cd
littlesat <littlesat99@yahoo.com>
2017-03-04 12:55:17 +0100

    Revert "VolumeControl: Remove two lines that are not required"
    This reverts commit 1025da1b81baed5e1712515a20365974a6addbbb.

--- lib/python/Components/VolumeControl.py
+++ lib/python/Components/VolumeControl.py
@@ -47,10 +47,32 @@ class VolumeControl:
 		config.audio.volume.save()
 
 	def volUp(self):
-		self.setVolume(+1)
+		vol = self.volctrl.getVolume()
+		if vol < 3:
+			vol += 1
+		elif vol < 9:
+			vol += 2
+		elif vol < 18:
+			vol += 3
+		elif vol < 30:
+			vol += 4
+		else:
+			vol += 5
+		self.setVolume(vol)
 
 	def volDown(self):
-		self.setVolume(-1)
+		vol = self.volctrl.getVolume()
+		if vol <= 3:
+			vol -= 1
+		elif vol <= 9:
+			vol -= 2
+		elif vol <= 18:
+			vol -= 3
+		elif vol <= 30:
+			vol -= 4
+		else:
+			vol -= 5
+		self.setVolume(vol)
 
 	def setVolume(self, newvol):
 		self.volctrl.setVolume(newvol, newvol)
