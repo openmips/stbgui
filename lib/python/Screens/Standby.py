3ab7727cdbb6ddedcac795cbf8a9f768b05011b1
littlesat <littlesat99@yahoo.com>
2017-03-20 13:43:32 +0100

    Standby: Use SystemInfo HasHDMI-CEC to check if hardware supports HDMI-CEC

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -335,7 +335,7 @@ class TryQuitMainloop(MessageBox):
 						Console().ePopen("/usr/script/Standby.sh off")
 					if os.path.exists("/usr/script/standby_enter.sh"):
 						Console().ePopen("/usr/script/standby_enter.sh")
-					if HardwareInfo().has_hdmi() and config.hdmicec.enabled.value and config.hdmicec.control_tv_standby.value and config.hdmicec.next_boxes_detect.value:
+					if SystemInfo["HasHDMI-CEC"] and config.hdmicec.enabled.value and config.hdmicec.control_tv_standby.value and config.hdmicec.next_boxes_detect.value:
 						import Components.HdmiCec
 						Components.HdmiCec.hdmi_cec.secondBoxActive()
 						self.delay = eTimer()
